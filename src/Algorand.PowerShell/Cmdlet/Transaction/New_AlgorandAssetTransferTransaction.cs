dc567421a877d4535cf9913ce7ed345b0fd61cc4
Geoff O'Donnell <odonne89@outlook.com>
2022-04-16 12:18:52 -0400

    Implements New_AlgorandAssetTransferTransaction

--- src/Algorand.PowerShell/Cmdlet/Transaction/New_AlgorandAssetTransferTransaction.cs
+++ src/Algorand.PowerShell/Cmdlet/Transaction/New_AlgorandAssetTransferTransaction.cs
@@ -1,18 +1,39 @@
-﻿using System;
+﻿using Algorand.PowerShell.Models;
 using System.Management.Automation;
 
 namespace Algorand.PowerShell.Cmdlet.Transaction {
+
 	[Cmdlet(VerbsCommon.New, "AlgorandAssetTransferTransaction")]
 	public class New_AlgorandAssetTransferTransaction : NewTransactionBase {
 
+		[Parameter(Mandatory = false)]
+		public ulong? XferAsset { get; set; }
+
+		[Parameter(Mandatory = false)]
+		public ulong? AssetAmount { get; set; }
+
+		[Parameter(Mandatory = false)]
+		public Address AssetSender { get; set; }
+
+		[Parameter(Mandatory = false)]
+		public Address AssetReceiver { get; set; }
+
+		[Parameter(Mandatory = false)]
+		public Address AssetCloseTo { get; set; }
+
 		protected override void ProcessRecord() {
 
-			WriteError(
-				new ErrorRecord(new NotImplementedException(), String.Empty, ErrorCategory.NotSpecified, this));
+			var result = CreateTransaction(TxType.KeyRegistration);
+
+			result.xferAsset = XferAsset;
+			result.assetAmount = AssetAmount;
+			result.assetSender = AssetSender;
+			result.assetReceiver = AssetReceiver;
+			result.assetCloseTo = AssetCloseTo;
+
+			WriteObject(result);
 		}
 
-
 	}
-
 
 }
