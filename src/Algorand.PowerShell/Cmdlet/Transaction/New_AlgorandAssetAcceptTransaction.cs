0042fde07e7be56d9e2b70058564d4c96d28b244
Geoff O'Donnell <odonne89@outlook.com>
2022-04-16 12:22:36 -0400

    Implements New_AlgorandAssetAcceptTransaction

--- src/Algorand.PowerShell/Cmdlet/Transaction/New_AlgorandAssetAcceptTransaction.cs
+++ src/Algorand.PowerShell/Cmdlet/Transaction/New_AlgorandAssetAcceptTransaction.cs
@@ -1,18 +1,31 @@
-﻿using System;
+﻿using Algorand.PowerShell.Models;
 using System.Management.Automation;
 
 namespace Algorand.PowerShell.Cmdlet.Transaction {
+
 	[Cmdlet(VerbsCommon.New, "AlgorandAssetAcceptTransaction")]
 	public class New_AlgorandAssetAcceptTransaction : NewTransactionBase {
 
+		[Parameter(Mandatory = false)]
+		public ulong? XferAsset { get; set; }
+
+		[Parameter(Mandatory = false)]
+		public Address AssetSender { get; set; }
+
+		[Parameter(Mandatory = false)]
+		public Address AssetReceiver { get; set; }
+
 		protected override void ProcessRecord() {
 
-			WriteError(
-				new ErrorRecord(new NotImplementedException(), String.Empty, ErrorCategory.NotSpecified, this));
+			var result = CreateTransaction(TxType.AssetTransfer);
+
+			result.xferAsset = XferAsset;
+			result.assetSender = AssetSender;
+			result.assetReceiver = AssetReceiver;
+
+			WriteObject(result);
 		}
 
-
 	}
-
 
 }
