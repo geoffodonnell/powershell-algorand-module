f7d240d409cc976c5f7630334be20fd07125149f
Geoff O'Donnell <odonne89@outlook.com>
2022-04-21 18:50:11 -0400

    Adds ConvertTo_AlgorandTransaction

---
+++ src/Algorand.PowerShell/Cmdlet/Transaction/ConvertTo_AlgorandTransaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Management.Automation;
+using SdkTransaction = Algorand.Transaction;
+
+namespace Algorand.PowerShell.Cmdlet.Transaction {
+
+	[Cmdlet(VerbsData.ConvertTo, "AlgorandTransaction")]
+	public class ConvertTo_AlgorandTransaction : CmdletBase {
+
+		[Parameter(
+			Mandatory = true,
+			ValueFromPipeline = true)]
+		public string Json { get; set; }
+
+		protected override void ProcessRecord() {
+
+			if (String.IsNullOrWhiteSpace(Json)) {
+				WriteError(new ErrorRecord(
+					new Exception($"{nameof(Json)} must be valid JSON object."),
+					String.Empty,
+					ErrorCategory.NotSpecified,
+					this));
+				return;
+			}
+
+			var result = Encoder.DecodeFromJson<SdkTransaction>(Json);
+
+			WriteObject(result);
+		}
+
+	}
+
+}
