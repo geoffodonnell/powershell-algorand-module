814c8a54d338a4bf03d23098903e8ec28787c8e6
Geoff O'Donnell <odonne89@outlook.com>
2022-04-24 20:29:49 -0400

    Updates dev CI pipeline

--- src/create-module-manifest.ps1
+++ src/create-module-manifest.ps1
@@ -2,9 +2,7 @@
 param (
     [Parameter(Position = 0, mandatory = $true)]
     [string] $Path,
-    [Parameter(Position = 2, mandatory = $false)]
-    [string] $Version = "1.0.0",
-    [Parameter(Position = 4, mandatory = $false)]
+    [Parameter(Position = 1, mandatory = $false)]
     [string] $Guid = "c66dcc66-564b-45c8-8406-bac225fcdf02"
 )
 
@@ -14,16 +12,23 @@ if (-not (Test-Path -Path $Path -ErrorAction SilentlyContinue)){
 }
 
 $author = "Geoff O'Donnell"
+$rootModule = "Algorand.PowerShell.dll"
+
 $name = Get-Item -Path $Path | Select -ExpandProperty Name
 $moduleName = "$($Name).psd1";
 $modulePath = [System.IO.Path]::GetFullPath((Join-Path -Path $Path -ChildPath $moduleName))
+
+# Version the module based on the file version of the assembly
+$rootModulePath = [System.IO.Path]::GetFullPath((Join-Path -Path $Path -ChildPath $rootModule))
+$rootModuleInfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($rootModulePath)
+$version = $rootModuleInfo.FileVersion
 
 New-ModuleManifest -Author $author `
     -CmdletsToExport "*" `
     -CompanyName $author `
     -Description "Algorand Tools for PowerShell" `
     -Guid $Guid `
-    -ModuleVersion $Version `
+    -ModuleVersion $version `
     -Path $modulePath `
     -PowerShellVersion "7.0" `
-    -RootModule ".\Algorand.PowerShell.dll"
+    -RootModule $rootModule
